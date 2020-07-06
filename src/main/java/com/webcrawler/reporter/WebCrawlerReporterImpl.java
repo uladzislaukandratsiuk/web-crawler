c5ecdc9f859be3e064d891659ebb351a4b0d564c
Vladislav Kondratuk <vladislavkondratuk322@gmail.com>
2020-07-06 23:44:20 +0300

    refactoring reportLinksWithTopTenElementHits() in WebCrawlerReporterImpl class

--- src/main/java/com/webcrawler/reporter/WebCrawlerReporterImpl.java
+++ src/main/java/com/webcrawler/reporter/WebCrawlerReporterImpl.java
@@ -27,7 +27,7 @@ public class WebCrawlerReporterImpl implements WebCrawlerReporter {
 
     @Override
     public Map<String, List<Integer>> reportLinksWithElementHits(List<String> linkElements) {
-        return countLinksElementHits(linkElements);
+        return getLinksWithElementHits(linkElements);
     }
 
     @Override
@@ -35,7 +35,7 @@ public class WebCrawlerReporterImpl implements WebCrawlerReporter {
         return getLinksWithTopTenElementHits(linkElements);
     }
 
-    private Map<String, List<Integer>> countLinksElementHits(List<String> linkElements) {
+    private Map<String, List<Integer>> getLinksWithElementHits(List<String> linkElements) {
 
         Map<String, List<Integer>> linkElementHits = new HashMap<>();
         Set<String> links = webCrawler.crawlLinkWithDepth();
@@ -43,9 +43,9 @@ public class WebCrawlerReporterImpl implements WebCrawlerReporter {
         for (String link : links) {
 
             int totalHits;
-            int termHits = 0;
+            int elementHits;
 
-            List<Integer> elementHits = new ArrayList<>();
+            List<Integer> listOfElementHits = new ArrayList<>();
 
             try {
                 List<String> textStrings = new ArrayList<>();
@@ -57,43 +57,51 @@ public class WebCrawlerReporterImpl implements WebCrawlerReporter {
 
                     pageElements.forEach(pageElement -> textStrings.add(pageElement.text()));
 
-                    termHits += textStrings.stream()
-                            .filter(text ->
-                                    text.matches(".*\\b(\\w*" + element + "\\w*)\\b.*"))
-                            .count();
+                    elementHits = countElementHits(textStrings, element);
 
-                    elementHits.add(termHits);
-                    termHits = 0;
+                    listOfElementHits.add(elementHits);
                 }
 
             } catch (IOException | IllegalArgumentException e) {
                 log.error("{}", e.getMessage());
             }
 
-            totalHits = elementHits.stream().mapToInt(hits -> hits).sum();
-            elementHits.add(totalHits);
-            linkElementHits.put(link, elementHits);
+            totalHits = countTotalHits(listOfElementHits);
+
+            listOfElementHits.add(totalHits);
+            linkElementHits.put(link, listOfElementHits);
         }
 
         return linkElementHits;
     }
 
+    private int countElementHits(List<String> textStrings, String element) {
+        return (int) textStrings.stream()
+                .filter(text -> text
+                        .matches(".*\\b(\\w*" + element + "\\w*)\\b.*"))
+                .count();
+    }
+
+    private int countTotalHits(List<Integer> listOfElementHits) {
+        return listOfElementHits.stream().mapToInt(hits -> hits).sum();
+    }
+
     private Map<String, List<Integer>> getLinksWithTopTenElementHits(List<String> linkElements) {
 
         Map<String, List<Integer>> linksAndHits =
-                countLinksElementHits(linkElements);
+                getLinksWithElementHits(linkElements);
 
         linksAndHits = linksAndHits
                 .entrySet()
                 .stream()
                 .sorted((hit1, hit2) ->
                         (hit2.getValue().get(hit2.getValue().size() - 1))
-                        .compareTo((hit1.getValue().get(hit1.getValue().size() - 1))))
+                                .compareTo((hit1.getValue().get(hit1.getValue().size() - 1))))
                 .limit(10)
                 .collect(toMap(Map.Entry::getKey, Map.Entry::getValue,
                         (k, v) -> v, LinkedHashMap::new));
 
-        log.info("Top Ten");
+        log.info("Top Ten Links With Terms Hits:");
         linksAndHits.forEach((link, elementHits) ->
                 log.info("{} {}", link, elementHits));
 
